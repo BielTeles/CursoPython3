fa7f59968b2b330d9da72387a9c5608cf5953827
Dev Teles <66977592+BielTeles@users.noreply.github.com>
2025-01-30 16:48:14 -0300

    Delete desafio011.py

--- desafio011.py
+++
@@ -1,6 +0,0 @@
-l = float(input('Digite a largura da parede: '))
-h = float(input('Digite a altura da parede: '))
-a = h*l
-paint = a/2
-print('Você irá precisar de {:.2f}L de tinta'.format(paint))
-#Concluido
